b5acb22d73bfe258f59a571f6545ec6c4d6f99be
bahaagabal <bgabal93@gmail.com>
2025-05-17 18:45:52 +0300

    First Section Init the App plus building the architecture in addition to implementing search bar and the Book List Screen

---
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListAction.kt
@@ -0,0 +1,13 @@
+package com.plcoding.bookpedia.book.presentation.book_list
+
+import com.plcoding.bookpedia.book.domain.Book
+
+sealed interface BookListAction {
+
+    data class OnSearchQueryChange(val query: String) : BookListAction
+
+    data class OnBookItemClicked(val book: Book) : BookListAction
+
+    data class OnTabSelected(val index: Int) : BookListAction
+
+}
